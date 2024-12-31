b15c7b800e800412e6ba63666078b527860159f9
tuleshghelani <ghelanitulesh@gmail.com>
2024-12-31 15:29:12 +0530

    Updated build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://common-backend-six.vercel.app'
+  // baseURL: 'https://common-backend-six.vercel.app',
+  baseURL: 'https://neomat-backend.vercel.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // baseURL: 'http://192.168.1.8:3000'  
-  baseURL: 'https://common-backend-six.vercel.app'
+  // baseURL: 'https://common-backend-six.vercel.app'
+  baseURL: 'https://neomat-backend.vercel.app'
 };
